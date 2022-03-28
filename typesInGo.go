c3c44e4ddc60da9694f69b5954fc7bc05c6c6abc
ILikeTea <ybingcheng@gmail.com>
2022-03-28 16:34:31 +0800

    update

--- typesInGo.go
+++ typesInGo.go
@@ -4,6 +4,11 @@ package main
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	text := "The following isthe account information."
+	firstName := "Luke"
+	age := 20
+	height := 1.72
+	fmt.Printf(“%T %T %T %T\n”, text, firstName, age, height)
 }
 
 func main() {
